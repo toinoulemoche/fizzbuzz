e96521219b6238d2543948bd0f5bb0d737f9baa9
toinoulemoche <avaurs28@gmail.com>
2022-04-29 13:27:29 +0200

    fin de l'exercice buzzfizz (correction de syntaxe)

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -2,16 +2,22 @@ const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
 emitter.on('fizzBuzz', function(args){
-	const chaine = "";
+	var chaine = "";
 	if(args % 3 == 0){
                 chaine = chaine + "Fizz";
-        {
-	else if(args % 5 == 0){
+        }
+	if(args % 5 == 0){
                 chaine = chaine + "Buzz";
-        {
-	else{
-		chaine = args;
+        }
+	if(chaine == ""){
+		console.log(args);
 	}
-	console.log(chaine);
-}
+	else{
+		console.log(chaine);
+	}
+});
 
+emitter.emit("fizzBuzz", 15);
+emitter.emit("fizzBuzz", 8);
+emitter.emit("fizzBuzz",9);
+emitter.emit("fizzBuzz",20);
