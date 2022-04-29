c33d3c470e404af952924981deba19a002e2c9a4
toinoulemoche <avaurs28@gmail.com>
2022-04-29 12:57:03 +0200

    début exercice 5

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -1,1 +1,17 @@
-////
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
+emitter.on('fizzBuzz', function(args){
+	const chaine = "";
+	if(args % 3 == 0){
+                chaine = chaine + "Fizz";
+        {
+	else if(args % 5 == 0){
+                chaine = chaine + "Buzz";
+        {
+	else{
+		chaine = args;
+	}
+	console.log(chaine);
+}
+
